f410cc5c8f2a4f2cf1a384969f960de0ee211d0d
winigl <winstongl2000@gmail.com>
2023-09-03 22:18:06 -0300

    Agregue el método stopPropagation( )

--- eventos.js
+++ eventos.js
@@ -1,8 +1,14 @@
 var divconteiner = document.getElementById('myDiv')
+var boton = document.getElementById('myButtom');
 
 divconteiner.style.padding = ('5px')
 divconteiner.style.backgroundColor = ('green')
 
-divconteiner.addEventListener("click" , function() {
+divconteiner.addEventListener("click" , function(event) {
     alert('Hola soy el div!');
 });
+
+boton.addEventListener("click", function(event) {
+    event.stopPropagation();
+    alert('Hola!');
+  });
