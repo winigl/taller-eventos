066ae887193dbc51db98bc237a8add8f8521d96f
winigl <winstongl2000@gmail.com>
2023-09-03 21:36:30 -0300

    Agrego función del div

---
+++ eventos.js
@@ -0,0 +1,8 @@
+var divconteiner = document.getElementById('myDiv')
+
+divconteiner.style.padding = ('5px')
+divconteiner.style.backgroundColor = ('green')
+
+divconteiner.addEventListener("click" , function() {
+    alert('Hola soy el div!');
+});
